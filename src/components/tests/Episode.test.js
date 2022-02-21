6bef11a74c43b3cc2b39f600581640acd56882e7
Schuyler <skym1n0r91@gmail.com>
2022-02-21 15:17:55 -0600

    initial commit

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Episode from './../Episode';
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Episode from "./../Episode";
 
-test("renders without error", () => { });
+test("renders without error", () => {});
 
-test("renders the summary test passed as prop", () => { });
+test("renders the summary test passed as prop", () => {});
 
-test("renders default image when image is not defined", () => { });
+test("renders default image when image is not defined", () => {});
